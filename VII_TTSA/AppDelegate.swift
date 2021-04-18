b960575e0657639877e8f2542fae804781f15e24
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-04-18 10:56:04 +0400

    Initial Commit

---
+++ VII_TTSA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VII_TTSA
+//
+//  Created by Muhammad Saud Anwar on 18/04/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VII_TTSA/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VII_TTSA
+//
+//  Created by Muhammad Saud Anwar on 18/04/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
