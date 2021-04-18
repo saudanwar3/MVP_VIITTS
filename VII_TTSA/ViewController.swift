2ac3954039dc02d7269c720a08c462bbc8faf42a
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-04-18 14:16:32 +0400

    create login/signup view

--- VII_TTSA/ViewController.swift
+++ VII_TTSA/ViewController.swift
@@ -10,11 +10,57 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet var mainView: NSView!
+    @IBOutlet weak var separatorHorizontalView: NSView!
+    @IBOutlet weak var separatorHorizontalView3: NSView!
+    @IBOutlet weak var separatorHorizontalView2: NSView!
+    
+    @IBOutlet weak var emailView: NSView!
+    @IBOutlet weak var emailTxtField: NSTextField!
+    
+    @IBOutlet weak var passwordView: NSView!
+    @IBOutlet weak var passwordTxtField: NSTextField!
+    
+    @IBOutlet weak var signinBox: NSBox!
+    @IBOutlet weak var signinBtn: NSButton!
+    
+    @IBOutlet weak var keepMeSignedinBtn: NSButton!
+    
+    @IBOutlet weak var signupBox: NSBox!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        self.mainView.wantsLayer = true
-        self.mainView.layer?.backgroundColor = .white
+        self.view.window?.setFrame(NSRect(x:0,y:0,width: 1440,height: 790), display: true)
+
+        self.addColorToView(view: self.mainView, color: .white)
+        self.addColorToView(view: self.separatorHorizontalView, color: .black)
+        self.addColorToView(view: self.separatorHorizontalView3, color: .black)
+        self.addColorToView(view: self.separatorHorizontalView2, color: .black)
+        
+        self.addColorToView(view: self.emailView, color: NSColor(hex: "#2A2A36")!)
+        self.emailView.layer?.borderColor = .black
+        self.emailView.layer?.cornerRadius = 5.0
+        self.emailView.layer?.borderWidth = 1.0
+        
+        self.addColorToView(view: self.passwordView, color: NSColor(hex: "#2A2A36")!)
+        self.passwordView.layer?.borderColor = .black
+        self.passwordView.layer?.cornerRadius = 5.0
+        self.passwordView.layer?.borderWidth = 1.0
+
+
+        self.signinBox.cornerRadius = 18
+        
+        self.signupBox.wantsLayer = true
+        self.signupBox.cornerRadius = 18
+        self.signupBox.borderColor = NSColor(hex: "#2A2A36")!
+        self.signupBox.borderWidth = 2.0
+        self.signupBox.layer?.backgroundColor = NSColor(hex: "#2A2A36")!.cgColor
+        self.signupBox.layer?.cornerRadius = 18.0
+        self.signupBox.layer?.borderWidth = 2.0
+        self.signupBox.layer?.borderColor = NSColor(hex: "#2A2A36")!.cgColor
+        self.signupBox.layer?.masksToBounds = true
+        
+        
     }
 
     override var representedObject: Any? {
@@ -23,6 +69,17 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func keepmeSignedInBtnClicked(_ sender: NSButton) {
+        print("clicked")
+    }
+    
 }
+extension ViewController
+{
+    func addColorToView(view:NSView , color:NSColor) -> Void {
+        view.wantsLayer = true
+//        view.layer?.backgroundColor = color.cgColor
+        view.setValue(color, forKey: "backgroundColor")
 
+    }
+}
