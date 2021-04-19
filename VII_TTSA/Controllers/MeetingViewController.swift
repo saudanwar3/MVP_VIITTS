537e0b1c71500766dbd01a8f6155d270ba7cf207
Muhammad Saud Anwar <saudanwar3@hotmail.com>
2021-04-19 15:58:30 +0400

    Added new controller

---
+++ VII_TTSA/Controllers/MeetingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingViewController.swift
+//  VII_TTSA
+//
+//  Created by Muhammad Saud Anwar on 19/04/2021.
+//
+
+import Cocoa
+
+class MeetingViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
